9cabf37c7d70854eeff5342539606061f8e39312
Fung Wei Tan Tai <fungwei@goget.my>
2014-11-05 13:54:23 +0800

    account creation and validation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,9 +2,5 @@ use Rack::Flash
 
 get '/' do
   # Look in app/views/index.erb
-  if session[:user_id] == nil
-    redirect '/login'
-  else
     erb 'albums/list'
-  end
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,6 +2,11 @@ get '/login' do
   erb :'users/new'
 end
 
+delete '/logout' do
+  session[:user_id] = nil
+  redirect "/"
+end
+
 post '/users' do
   if params[:submit] == "create"
     @user = User.new(username: params[:username], password: params[:password])
@@ -11,9 +16,9 @@ post '/users' do
   else
     # byebug
     if User.valid?(params[:username])
-    @user = User.where(username: params[:username]).first
-      if @user.authenticate(params[:password])
-        session[:user_id] = @user.id
+    user = User.where(username: params[:username]).first
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect "/"
       else
         flash[:notice] = "Wrong Password"
